d532e9f133479f7b0ed9dbfcb4d3773c8b4ce34f
valentinagalvis <danniittasanchez@gmail.com>
2025-11-13 20:45:22 -0500

    agregar

---
+++ src/routes/animal.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const animalSchema = require("../models/animalModel");
+
+//Nuevo animal
+router.post("/animals", (req, res) => {
+    const animal = animalSchema(req.body);
+    animal
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/animals", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.put("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+module.exports = router;
